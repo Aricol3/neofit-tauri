b60d611290e35acf5366b4d7efbe30a497507088
Adrian <xxaricolxx@gmail.com>
2025-03-11 13:47:36 +0200

    water logic

--- src/components/Water.tsx
+++ src/components/Water.tsx
@@ -4,16 +4,23 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import { Card, CardBody, CardHeader } from "@heroui/react";
 import WaterCup from "./WaterCup.tsx";
+import { useState } from "react";
 
 const Water = () => {
+  const [filledCups, setFilledCups] = useState(0);
   const totalCups = 12;
-  const filledCups = 3;
+  const cupSize = 0.25;
+
+  const onWaterClick = (index: number) => {
+    if (index >= filledCups) setFilledCups(index + 1);
+    else setFilledCups(index);
+  };
 
   return (
     <Card className="w-full h-[150px] border-none bg-waterBackground" shadow="none">
       <CardHeader className="pb-0 flex flex-row justify-between">
         <p className="text-white text-lg font-[600]">Water</p>
-        <p className="text-white text-sm font-[600]">0,75 / 3,5 l</p>
+        <p className="text-white text-sm font-[600]">{filledCups * cupSize} / {totalCups * cupSize} l</p>
       </CardHeader>
       <CardBody className="flex flex-row pt-5 pb-1">
         <Swiper
@@ -30,6 +37,7 @@ const Water = () => {
               <WaterCup
                 isFilled={index < filledCups}
                 isNextToFill={index === filledCups}
+                onClick={() => onWaterClick(index)}
               />
             </SwiperSlide>
           ))}
--- src/components/WaterCup.tsx
+++ src/components/WaterCup.tsx
@@ -3,11 +3,12 @@ import "./WaterCup.scss";
 interface WaterCupProps {
   isFilled: boolean;
   isNextToFill?: boolean;
+  onClick: () => void;
 }
 
-const WaterCup = ({ isFilled, isNextToFill }: WaterCupProps) => {
+const WaterCup = ({ isFilled, isNextToFill, onClick }: WaterCupProps) => {
   return (
-    <div className="flex flex-col justify-center items-center relative">
+    <div className="flex flex-col justify-center items-center relative" onClick={onClick}>
       <div className="top-cup"></div>
       <div className="cup relative">
         {isFilled ? <div className="water filled"></div> : isNextToFill ? <div className="plus"></div> : null}
